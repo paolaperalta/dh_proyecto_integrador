7355f83d0b1c0667828d49ca2fc19164eeddac53
paolaperalta <peraltapaola@gmail.com>
2019-04-06 17:38:14 -0300

    version final del login con estilo y en formato php

---
+++ login.php
@@ -0,0 +1,56 @@
+
+
+
+
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <title>Acceder</title
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+    <meta charset= "utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css?family=Lato:100,300|Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="CSS/style.css">
+
+
+    </head>
+        <div class="container">
+        <h1>INICIAR SESIÓN</h1>
+        <br>
+        <label for="email" class="etiquetas"><b>EMAIL</b></label>
+        <input class="completar" type="email" placeholder="Ingrese su email" name="email" required>
+        <br><br>
+        <label for="psw" class="etiquetas"><b>CONTRASEÑA</b></label>
+        <input class="completar" type="password" placeholder="Ingrese su contraseña" name="psw" required>
+        <br>
+        <p><a class="olvidaste" style=color:#DFB988>¿Olvidaste tu contraseña?</a></p>
+
+
+        <button type="button" class="btn btn-primary" data-toggle="button" aria-disabled="true" autocomplete="off">
+          Iniciar sesión
+        </button>
+
+
+        <br><br>
+          <div class="recordar">
+          <input type="checkbox" name="remember" style="margin:0px 3px 0px 0px">Recordarme
+          </div>
+
+
+</div>
+
+        <br>
+        <br>
+        <label>
+
+        </label>
+
+    <meta charset="utf-8">
+    <title></title>
+
+  <body>
+
+  </body>
+</html>
